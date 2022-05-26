02525534bd5dfde92c5dcd30e88582961d6ba0b3
kedeinroga <keybroga@gmail.com>
2022-05-26 17:15:05 -0500

    Stock interface and mocks

---
+++ mocks/mockups.ts
@@ -0,0 +1,66 @@
+import { StockRegistry } from "../src/interfaces";
+
+const mockupStock: StockRegistry[] = [
+  {
+    date: new Date("2022-05-30"),
+    price: 50,
+  },
+  {
+    date: new Date("2022-04-29"),
+    price: 45,
+  },
+  {
+    date: new Date("2022-04-28"),
+    price: 42,
+  },
+  {
+    date: new Date("2022-03-21"),
+    price: 43,
+  },
+  {
+    date: new Date("2022-01-11"),
+    price: 41,
+  },
+  {
+    date: new Date("2022-01-09"),
+    price: 37,
+  },
+  {
+    date: new Date("2022-01-22"),
+    price: 40,
+  },
+  {
+    date: new Date("2022-01-12"),
+    price: 35,
+  },
+  {
+    date: new Date("2021-06-30"),
+    price: 32,
+  },
+  {
+    date: new Date("2021-09-30"),
+    price: 30,
+  },
+  {
+    date: new Date("2021-07-07"),
+    price: 24,
+  },
+  {
+    date: new Date("2020-07-30"),
+    price: 22,
+  },
+  {
+    date: new Date("2020-06-30"),
+    price: 20,
+  },
+  {
+    date: new Date("2020-05-30"),
+    price: 19,
+  },
+  {
+    date: new Date("2020-04-30"),
+    price: 15,
+  },
+];
+
+export { mockupStock };
---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface StockRegistry {
+  date: Date;
+  price: number;
+}
+
